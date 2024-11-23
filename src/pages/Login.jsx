685d74b7863a8268ebc4aca4cb080c949d0fa51d
Bernard <Bernard.koech@student.moringaschool.com>
2024-11-24 00:11:23 +0300

    reduced size of title

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -28,7 +28,7 @@ const Login = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100 mt-3">
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 ">
       <div className="bg-white shadow-lg rounded-lg p-8 max-w-sm w-full">
         <h2 className="text-2xl font-bold text-gray-800 text-center mb-6">Login</h2>
         {error && <p className="text-red-500 text-sm mb-4">{error}</p>}
